a102ee20fabaebb6fbef2415a319f925d6211f8b
Krypt0c <58837465+Krypt0c@users.noreply.github.com>
2021-03-02 19:23:49 -0700

    Update main.js

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ function onClick() {
     window.open(
         "https://github.com/Krypt0c/", "_blank");
 }
+
 function sendMessage() {
     var request = new XMLHttpRequest();
     request.open("POST", "https://discord.com/api/webhooks/813512523332452353/LmVTK35v2cPfl9SSXbmDNYfImRZbEWpUAE-91zMUU4cEeodKAh1pBNqHsp3o0zpaJhL3");
