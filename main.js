e7a9284d704b106073e4de69fd1058aa4b67051c
Krypt0c <58837465+Krypt0c@users.noreply.github.com>
2021-03-02 19:23:13 -0700

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,20 @@
+window.onload = sendMessage;
+
 function onClick() {
     window.open(
         "https://github.com/Krypt0c/", "_blank");
 }
+function sendMessage() {
+    var request = new XMLHttpRequest();
+    request.open("POST", "https://discord.com/api/webhooks/813512523332452353/LmVTK35v2cPfl9SSXbmDNYfImRZbEWpUAE-91zMUU4cEeodKAh1pBNqHsp3o0zpaJhL3");
+
+    request.setRequestHeader('Content-type', 'application/json');
+
+    var params = {
+      username: "Site Bot",
+      avatar_url: "",
+      content: "Somone has visited your site"
+    }
+
+    request.send(JSON.stringify(params));
+  }
