60a1b2cd0367ce272875305327695e6d796bf90d
Krypt0c <58837465+Krypt0c@users.noreply.github.com>
2021-03-09 08:38:37 -0700

    update

--- main2.js
+++ main2.js
@@ -4,12 +4,15 @@ function onClick() {
   }
   function onClick2() {
     window.open(
-        "contact.html", "_blank");
+        "https://github.com/Krypt0c", "_blank");
   }
-  function onClick3() {
-    window.open(
-        "contact.html", "_blank");
-  }
+    function onClick3() {
+            var copyText = document.getElementById("copyMe");
+            copyText.select();
+            copyText.setSelectionRange(0, 99999)
+            document.execCommand("copy");
+            alert("Copied the text: " + copyText.value);
+      }
   function onClick4() {
     window.open(
         "contact.html", "_blank");
