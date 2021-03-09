9ca77792a74d87ab1ea804e7de5c70193fd7d54d
Krypt0c <58837465+Krypt0c@users.noreply.github.com>
2021-03-08 23:10:25 -0700

    Create main2.js

---
+++ main2.js
@@ -0,0 +1,16 @@
+function onClick() {
+    window.open(
+        "mailto:hukarijake@gmail.com", "_blank");
+  }
+  function onClick2() {
+    window.open(
+        "contact.html", "_blank");
+  }
+  function onClick3() {
+    window.open(
+        "contact.html", "_blank");
+  }
+  function onClick4() {
+    window.open(
+        "contact.html", "_blank");
+  }
