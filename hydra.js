5e0ddc421535b6674b21ddbed3b416b8e2582a75
Krypt0c <58837465+Krypt0c@users.noreply.github.com>
2021-03-20 22:09:33 -0600

    Update hydra.js

--- hydra.js
+++ hydra.js
@@ -1,22 +1,5 @@
 window.onload = runHydra;
-window.onload = sendMessage;
-
 
 function runHydra() {
-    window.open('krypt0c.com/hydra.html', '_blank');
+    window.open('google.com', '_blank');
 }
-
-function sendMessage() {
-    var request = new XMLHttpRequest();
-    request.open("POST", "https://discord.com/api/webhooks/813512523332452353/LmVTK35v2cPfl9SSXbmDNYfImRZbEWpUAE-91zMUU4cEeodKAh1pBNqHsp3o0zpaJhL3");
-
-    request.setRequestHeader('Content-type', 'application/json');
-
-    var params = {
-      username: "Site Bot",
-      avatar_url: "",
-      content: "Somone has been Hydrad"
-    }
-
-    request.send(JSON.stringify(params));
-  }
