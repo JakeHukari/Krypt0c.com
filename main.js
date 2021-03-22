334b264b4a82d7eb6ede98bb870251e8b4a0e6e3
Krypt0c <58837465+Krypt0c@users.noreply.github.com>
2021-03-22 14:57:27 -0600

    WebHook update

--- main.js
+++ main.js
@@ -16,7 +16,7 @@ function onClick3() {
 
 function sendMessage() {
     var request = new XMLHttpRequest();
-    request.open("POST", "https://discord.com/api/webhooks/813512523332452353/LmVTK35v2cPfl9SSXbmDNYfImRZbEWpUAE-91zMUU4cEeodKAh1pBNqHsp3o0zpaJhL3");
+    request.open("POST", "https://discord.com/api/webhooks/823660948270088222/SnN7fkfo_aT_S5xOVQ4PIzVjTliH97DHBky6BsaYT3mCNpHTSzu8JhLDQz76SWR0l-rn");
 
     request.setRequestHeader('Content-type', 'application/json');
 
