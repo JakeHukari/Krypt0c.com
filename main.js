e73b50033fc8f6732a7c12fdd3e946822d0432b5
Krypt0c <58837465+Krypt0c@users.noreply.github.com>
2021-04-09 09:20:36 -0600

    Update main.js

--- main.js
+++ main.js
@@ -15,13 +15,16 @@ function onClick3() {
 }
 
 function sendMessage() {
-    var request = new XMLHttpRequest();
-    request.open("POST", "https://discord.com/api/webhooks/823660948270088222/SnN7fkfo_aT_S5xOVQ4PIzVjTliH97DHBky6BsaYT3mCNpHTSzu8JhLDQz76SWR0l-rn");
+  var request = new XMLHttpRequest();
+  request.open("POST", "https://discord.com/api/webhooks/823660948270088222/SnN7fkfo_aT_S5xOVQ4PIzVjTliH97DHBky6BsaYT3mCNpHTSzu8JhLDQz76SWR0l-rn");
 
-    request.setRequestHeader('Content-type', 'application/json');
+  request.setRequestHeader('Content-type', 'application/json');
 
-    var params = {
-      username: "Site Bot",
-      avatar_url: "",
-      content: "Somone has visited your site"
-    }
+  var params = {
+    username: "Site Bot",
+    avatar_url: "",
+    content: "Somone has visited your site"
+  }
+
+  request.send(JSON.stringify(params));
+}
