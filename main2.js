48ab653a9efc9a168e9a6d97a96865428192667a
Krypt0c <58837465+Krypt0c@users.noreply.github.com>
2021-04-27 13:41:40 -0600

    Update main2.js

--- main2.js
+++ main2.js
@@ -1,12 +1,10 @@
 window.onload = sendMessage;
 
 function onClick() {
-    window.open(
-        "mailto:hukarijake@gmail.com", "_blank");
+    window.open("mailto:hukarijake@gmail.com", "_blank");
   }
   function onClick2() {
-    window.open(
-        "https://github.com/Krypt0c", "_blank");
+    window.open("https://github.com/Krypt0c", "_blank");
   }
   function onClick3() {
     window.open(
