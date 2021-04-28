96d11dd069c88de472a1f3e49fb0c44958b2758e
Krypt0c <58837465+Krypt0c@users.noreply.github.com>
2021-04-27 21:49:25 -0600

    Update main.js

--- main.js
+++ main.js
@@ -1,15 +1,12 @@
 function onClick() {
-      window.location.replace("krypt0c.com/about")}
-
-function onClick2() 
-    window.location.replace("krypt0c.com/contact")}
-
-function onClick3() {
-    window.location.replace("krypt0c.com/hire")
-}
-function Home() {
-    window.location.replace("krypt0c.com")
-  
-function onLoad() {
-console.log("test")
-}
+    window.location.replace("mailto:hukarijake@gmail.com", "_blank");
+  }
+  function onClick2() {
+    window.open("https://github.com/Krypt0c", "_blank");
+  }
+  function onClick3() {
+    window.open("https://twitter.com/Krypt0c", "_blank");
+  }
+  function Home() {
+    window.location.replace("index.html")
+  }
