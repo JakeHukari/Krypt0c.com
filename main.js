0f9e56d0ca9b0c815c7e8c0ac3121eee4bfdfc7a
Krypt0c <58837465+Krypt0c@users.noreply.github.com>
2021-04-27 21:44:18 -0600

    update

--- main.js
+++ main.js
@@ -9,20 +9,7 @@ function onClick3() {
 }
 function Home() {
     window.location.replace("krypt0c.com")
+  
+function onLoad() {
+console.log("test")
 }
-/*
-function sendMessage() {
-  var request = new XMLHttpRequest();
-  request.open("POST", "https://discord.com/api/webhooks/835027613834805268/gFEgtAJkiIb-rdpoUFVPUL4Y3-oZSLp4gI262SHUI6_1gSLryfbpxAsqG-Fn0rSlMAhQ");
-
-  request.setRequestHeader('Content-type', 'application/json');
-
-  var params = {
-    username: "Site Bot",
-    avatar_url: "",
-    content: "Somone has visited your site"
-  }
-
-  request.send(JSON.stringify(params));
-}
-*/
--- main2.js
+++ main2.js
@@ -1,5 +1,3 @@
-window.onload = sendMessage;
-
 function onClick() {
     window.open("mailto:hukarijake@gmail.com", "_blank");
   }
@@ -16,19 +14,3 @@ function onClick() {
   function Home() {
       window.location.replace("index.html")
   }
-  /*
-  function sendMessage() {
-    var request = new XMLHttpRequest();
-    request.open("POST", "https://discord.com/api/webhooks/835027613834805268/gFEgtAJkiIb-rdpoUFVPUL4Y3-oZSLp4gI262SHUI6_1gSLryfbpxAsqG-Fn0rSlMAhQ");
-
-    request.setRequestHeader('Content-type', 'application/json');
-
-    var params = {
-      username: "Site Bot",
-      avatar_url: "",
-      content: "Somone has visited your contact page"
-    }
-
-    request.send(JSON.stringify(params));
-  }
-  */
