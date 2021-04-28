04e5968295777cd560fd87d86256b10304fae669
Krypt0c <58837465+Krypt0c@users.noreply.github.com>
2021-04-27 21:50:35 -0600

    Update main.js

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 function onClick() {
-    window.location.replace("mailto:hukarijake@gmail.com", "_blank");
+    window.location.replace("about", "_blank");
   }
   function onClick2() {
-    window.location.replace("https://github.com/Krypt0c", "_blank");
+    window.location.replace("contact", "_blank");
   }
   function onClick3() {
     window.location.replace("https://twitter.com/Krypt0c", "_blank");
