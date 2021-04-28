76b0d07756ef8551f2d488631527af9215fb45ab
Krypt0c <58837465+Krypt0c@users.noreply.github.com>
2021-04-27 21:54:26 -0600

    Update main.js

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 function onClick() {
-    window.location.replace("about", "_blank");
+    window.location.replace("about.html", "_blank");
   }
   function onClick2() {
-    window.location.replace("contact", "_blank");
+    window.location.replace("contact.html", "_blank");
   }
   function onClick3() {
     window.location.replace("hire", "_blank");
