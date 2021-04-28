75543bdc5e17ba1fb677eb53239cd43768f82ab2
Krypt0c <58837465+Krypt0c@users.noreply.github.com>
2021-04-27 21:44:25 -0600

    Update main3.js

--- main3.js
+++ main3.js
@@ -12,19 +12,3 @@ function onClick() {
   function Home() {
       window.location.replace("index.html")
   }
-  /*
-  function sendMessage() {
-    var request = new XMLHttpRequest();
-    request.open("POST", "https://discord.com/api/webhooks/835027613834805268/gFEgtAJkiIb-rdpoUFVPUL4Y3-oZSLp4gI262SHUI6_1gSLryfbpxAsqG-Fn0rSlMAhQ");
-
-    request.setRequestHeader('Content-type', 'application/json');
-
-    var params = {
-      username: "Site Bot",
-      avatar_url: "",
-      content: "Somone has visited your about page"
-    }
-
-    request.send(JSON.stringify(params));
-  }
-  */
