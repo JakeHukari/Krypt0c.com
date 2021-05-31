a898ba46fb729d773075e86eabe7ca6d6dbdda9b
Krypt0c <58837465+Krypt0c@users.noreply.github.com>
2021-05-31 14:01:35 -0600

    Update main.js

--- projects/node.js/main.js
+++ projects/node.js/main.js
@@ -1,11 +1,8 @@
 var http = require('http');
-var dt = require('./module');
+var dt = require('./myfirstmodule');
 
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/html'});
   res.write("The date and time is currently: " + dt.myDateTime());
   res.end();
 }).listen(8080);
-
-console.log('Server Running...');
-console.log('Listening on: ' + '8080' );
