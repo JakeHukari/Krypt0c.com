bfea6467e58da5fe83e436161b83399568db2a6e
Krypt0c <58837465+Krypt0c@users.noreply.github.com>
2021-05-31 13:52:03 -0600

    Update node.js

--- projects/node.js/node.js
+++ projects/node.js/node.js
@@ -4,3 +4,6 @@ http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
   res.end('Hello World!');
 }).listen(8080);
+
+console.log('This example is different!');
+console.log('The result is displayed in the Command Line Interface');
