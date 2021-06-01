2cc26c348c2bca6ee6fba92ae0c876618114a957
Krypt0c <58837465+Krypt0c@users.noreply.github.com>
2021-06-01 09:29:28 -0600

    Update main.js

--- projects/node.js/main.js
+++ projects/node.js/main.js
@@ -1,5 +1,6 @@
 var http = require('http');
 var fs = require('fs');
+var nodemailer = require('nodemailer');
 
 http.createServer(function (req, res) {
   fs.readFile('index.html', function(err, data) {
@@ -14,4 +15,27 @@ console.log('Server is running')
 fs.appendFile('testfile.txt', 'Test', function (err) {
   if (err) throw err;
   console.log('File is saved');
+});
+
+var transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: 'youremail@gmail.com',
+    pass: 'yourpassword'
+  }
+});
+
+var mailOptions = {
+  from: 'youremail@gmail.com',
+  to: 'myfriend@yahoo.com',
+  subject: 'Sending Email using Node.js',
+  text: 'That was easy!'
+};
+
+transporter.sendMail(mailOptions, function(error, info){
+  if (error) {
+    console.log(error);
+  } else {
+    console.log('Email sent: ' + info.response);
+  }
 });
