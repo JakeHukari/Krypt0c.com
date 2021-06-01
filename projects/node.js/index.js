3a3e95265353cbe6c9b23703b45879ebbe73c967
Krypt0c <58837465+Krypt0c@users.noreply.github.com>
2021-06-01 09:16:57 -0600

    Update index.js

---
+++ projects/node.js/index.js
@@ -0,0 +1,12 @@
+function onClick() {
+    window.location.replace("about.html", "_blank");
+  }
+  function onClick2() {
+    window.location.replace("contact.html", "_blank");
+  }
+  function onClick3() {
+    window.location.replace("hire.html", "_blank");
+  }
+  function Home() {
+    window.location.replace("index.html")
+  }
