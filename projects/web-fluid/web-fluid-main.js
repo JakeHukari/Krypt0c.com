b43ace3939edeb36a621a582c56567051e249598
Krypt0c <58837465+Krypt0c@users.noreply.github.com>
2021-06-03 22:54:11 -0600

    Update web-fluid-main.js

--- projects/web-fluid/web-fluid-main.js
+++ projects/web-fluid/web-fluid-main.js
@@ -158,7 +158,7 @@ function supportRenderTextureFormat (gl, internalFormat, format, type) {
 //GUI Functions
 function startGUI () {
     var gui = new dat.GUI({ width: 300 });
-    gui.add(config, 'DYE_RESOLUTION', { 'high': 1024, 'medium': 512, 'low': 256, 'very low': 128 }).name('Quality').onFinishChange(initFramebuffers);
+    gui.add(config, 'DYE_RESOLUTION', { 'high': 4096, 'higher': 2048, 'high': 1024, 'medium': 512, 'low': 256, 'very low': 128 }).name('Quality').onFinishChange(initFramebuffers);
     gui.add(config, 'SIM_RESOLUTION', { '32': 32, '64': 64, '128': 128, '256': 256, '512': 512 }).name('Simulation speed').onFinishChange(initFramebuffers);
     gui.add(config, 'DENSITY_DISSIPATION', 0, 4.0).name('Density Dissapation');
     gui.add(config, 'VELOCITY_DISSIPATION', 0, 4.0).name('Velocity Dissapation');
