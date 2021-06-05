72c21381952c73460557ec16069e5b8891d01abb
Krypt0c <58837465+Krypt0c@users.noreply.github.com>
2021-06-04 19:34:14 -0600

    Update web-fluid-main.js

--- projects/web-fluid/web-fluid-main.js
+++ projects/web-fluid/web-fluid-main.js
@@ -22,7 +22,7 @@ let config = {
     COLORFUL: true,
     COLOR_UPDATE_SPEED: 10,
     PAUSED: false,
-    BACK_COLOR: { r: 0, g: 0, b: 0 },
+    BACK_COLOR: { r: 0, g: 12, b: 24 },
     TRANSPARENT: false,
     BLOOM: true,
     BLOOM_ITERATIONS: 8,
